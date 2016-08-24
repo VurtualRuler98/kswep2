3332644c04c02a078b9e1f8e143e7da4a8c7dd4e
vurtual <VurtualRuler98@gmail.com>
2016-08-23 19:20:10 -0500

    added scout

---
+++ lua/weapons/weapon_kswep_scout/shared.lua
@@ -0,0 +1,58 @@
+--[[
+Copyright 2015 vurtual 
+VurtualRuler98@gmail.com
+vurtual.org
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+]]--
+
+
+if (SERVER) then
+	AddCSLuaFile("shared.lua")
+end
+
+if (CLIENT) then
+	SWEP.PrintName = "Steyr Scout"
+	SWEP.Author = "vurtual"
+	SWEP.Slot = 3
+	SWEP.SlotPos = 0
+end
+
+SWEP.Category = "Vurtual's K-Weapons"
+SWEP.Base = "weapon_kswep"
+SWEP.Primary.Delay = self.Owner:GetViewModel:SequenceDuration(ACT_VM_PRIMARYATTACK) 
+SWEP.Primary.Damage = 42
+SWEP.Primary.Spread = 0.0001
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.ViewModel = "models/weapons/cstrike/c_snip_scout.mdl"
+SWEP.WorldModel = "models/weapons/w_snip_scout.mdl"
+SWEP.Primary.Automatic = false
+SWEP.UseHands = true
+SWEP.Magazines = 4
+SWEP.MagSize = 10
+SWEP.Primary.ClipSize = SWEP.MagSize
+SWEP.Caliber = "vammo_762x51_M80"
+SWEP.Primary.Sound = Sound("weapon_scout.single")
+SWEP.ViewModelFlip = false
+SWEP.IronSightsPos = Vector( 0, 0, 0 )
+SWEP.IronSightsAng = Vector( 0, 0, 0 )
+SWEP.Secondary.Ammo = ""
+SWEP.CurrentlyReloading=0
+SWEP.ReloadAnimTime=0
+SWEP.RecoilControl=4
+SWEP.MaxMags = 4
+
+function SWEP:ReloadAct()
+	self:ReloadMag()	
+end
