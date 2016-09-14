123b67a404fa0dc676d430d34ce6456607b22e78
vurtual <VurtualRuler98@gmail.com>
2016-09-13 21:58:26 -0500

    kswepoptics moved to ins2

--- lua/autorun/kswep_shared.lua
+++ lua/autorun/kswep_shared.lua
@@ -6,13 +6,6 @@ end
 function AddKswepHands(tbl)
 	kswep_hands[tbl.name]=table.Copy(tbl)
 end
-function SetupKswepOptics()
-	kswep_hands = {}
-	hook.Run("kswepOptics")
-end
-if (kswep_optics==nil) then
-	SetupKswepOptics()
-end
 CreateConVar("kswep_slow",0,FCVAR_REPLICATED )
 CreateConVar("kswep_phys",1,FCVAR_REPLICATED )
 CreateConVar("kswep_max_flighttime",10,FCVAR_REPLICATED )
