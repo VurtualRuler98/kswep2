0be216eb41f084f31b7c9a91c1ca0433b1c2abf1
vurtual <VurtualRuler98@gmail.com>
2016-09-27 21:16:53 -0500

    Revert "Fixed trying to include file that does not exist, and including shared.lua twice"
    This reverts commit cce3f3ffd12d41642b3a8d0219d2fb98152af08e.

--- lua/entities/sent_vurt_ammo/init.lua
+++ lua/entities/sent_vurt_ammo/init.lua
@@ -1,7 +1,9 @@
 include('shared.lua')
 
 AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("vestbox.lua")
 AddCSLuaFile("shared.lua")
+include('shared.lua')
 
 function ENT:SpawnFunction(ply, tr)
 	if (!tr.HitWorld) then return end
