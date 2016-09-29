d6e3eacf561bcc8c9c27fcd41f594ebd1afedbc6
vurtual <VurtualRuler98@gmail.com>
2016-09-28 19:25:41 -0500

    Fixed useless include that made an error

--- lua/entities/sent_vurt_ammo/init.lua
+++ lua/entities/sent_vurt_ammo/init.lua
@@ -1,9 +1,7 @@
 include('shared.lua')
 
 AddCSLuaFile("cl_init.lua")
-AddCSLuaFile("vestbox.lua")
 AddCSLuaFile("shared.lua")
-include('shared.lua')
 
 function ENT:SpawnFunction(ply, tr)
 	if (!tr.HitWorld) then return end
