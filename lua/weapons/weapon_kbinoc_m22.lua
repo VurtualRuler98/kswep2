c4c622dd8285be041c0306bd47d3a92e8c98508e
vurtual <VurtualRuler98@gmail.com>
2016-10-11 14:02:46 -0500

    Improved rangefinder with binoc code and kswep ranging code, M22 binoculars

---
+++ lua/weapons/weapon_kbinoc_m22.lua
@@ -0,0 +1,22 @@
+if (SERVER) then
+	AddCSLuaFile()
+end
+if (CLIENT) then
+	SWEP.PrintName = "M22 Binoculars"
+	SWEP.Author = "vurtual"
+	SWEP.Slot = 3
+	SWEP.SlotPos = 0
+end
+SWEP.Base="weapon_kbinoc_base"
+SWEP.Category = "Vurtual's K-Weapons Equipment"
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.ViewModel = "models/weapons/v_pistol.mdl"
+SWEP.Magnification=7
+SWEP.Reticle="kswep/ret_m22_10.png"
+SWEP.MinMag=7
+SWEP.MaxMag=7
+SWEP.MagSteps=0
+SWEP.NoViewModel=true
+SWEP.PixelsPerMil=10
+SWEP.ReticlePixels=1024
---
+++ lua/weapons/weapon_kbinoc_rf.lua
@@ -0,0 +1,39 @@
+if (SERVER) then
+	AddCSLuaFile("shared.lua")
+end
+if (CLIENT) then
+	SWEP.PrintName = "KSwep Rangefinder"
+	SWEP.Author = "vurtual"
+	SWEP.Slot = 3
+	SWEP.SlotPos = 0
+end
+SWEP.Base="weapon_kbinoc_base"
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.Magnification=3
+SWEP.Overlay="effects/combine_binocoverlay"
+SWEP.Tripod=false
+SWEP.MinMag=3
+SWEP.MaxMag=12
+SWEP.MagSteps=4
+SWEP.NoViewModel=true
+SWEP.DrawCrosshair=true
+function SWEP:PrimaryAttack()
+	if (SERVER) then
+		local tr=util.TraceLine({
+			start=self.Owner:GetShootPos(),
+			endpos=self.Owner:GetShootPos()+self.Owner:GetAimVector()*78742,
+			filter=self.Owner,
+		} )
+		local dist=math.floor((tr.HitPos:Distance(tr.StartPos))/39.3701)
+		if (tr.Hit && !tr.HitSky) then
+			self.Owner:PrintMessage(HUD_PRINTCENTER,dist .. "m")
+		else
+			self.Owner:PrintMessage(HUD_PRINTCENTER,"---m")
+		end
+	end
+end
+function SWEP:CustomAmmoDisplay()	
+	return {}
+end
+
