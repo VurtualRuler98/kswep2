8f98a7bc358374718a01fd0097e5b2ed76a5310a
vurtual <VurtualRuler98@gmail.com>
2016-12-06 10:32:02 -0600

    Fix broken 545 ammo

--- lua/calibers/vammo_545x39.lua
+++ lua/calibers/vammo_545x39.lua
@@ -5,8 +5,8 @@ hook.Add("VurtualAmmotypes","vammo_545x39", function()
 	local tbl = {}
 	tbl.vestpenetration=KSWEP_ARMOR_III
 	tbl.dmgbase=10
-	tbl.powermin=5
-	tbl.powermax=8
+	tbl.dmgvitalmin=5
+	tbl.dmgvitalmax=8
 	tbl.name = "vammo_545x39_7n6m"
 	tbl.printname = "5.45x39mm 7N6M"
 	tbl.caliber = "vammo_545x39"
