4e0c235b673693abb16787c8a0e1f3e7a796e026
vurtual <VurtualRuler98@gmail.com>
2017-01-24 19:07:23 -0600

    G1 ballistics simulation

--- lua/weapons/weapon_kswep/shared.lua
+++ lua/weapons/weapon_kswep/shared.lua
@@ -2245,6 +2245,54 @@ function SWEP:DiscoverAnim(anim)
 	end
 	return nil
 end
+function SWEP:GetDrag(func,speed)
+	if (func~="G1") then func="G1" end --Always use G1 if nothing else, check to make sure it's not any other implemented function first though.
+	if (func=="G1") then
+		if (speed<400) then return 0.0176
+		elseif (speed<500) then return 0.0212
+		elseif (speed<600) then return 0.0249
+		elseif (speed<700) then return 0.0294
+		elseif (speed<800) then return 0.0361
+		elseif (speed<900) then return 0.0473
+		elseif (speed<1000) then return 0.0682
+		elseif (speed<1100) then return 0.1024
+		elseif (speed<1200) then return 0.1387
+		elseif (speed<1300) then return 0.1663
+		elseif (speed<1400) then return 0.1869
+		elseif (speed<1500) then return 0.2033
+		elseif (speed<1600) then return 0.2169
+		elseif (speed<1700) then return 0.2284
+		elseif (speed<1800) then return 0.2385
+		elseif (speed<1900) then return 0.2473
+		elseif (speed<2000) then return 0.2553
+		elseif (speed<2100) then return 0.2627
+		elseif (speed<2200) then return 0.2697
+		elseif (speed<2300) then return 0.2762
+		elseif (speed<2400) then return 0.2829
+		elseif (speed<2500) then return 0.2894
+		elseif (speed<2600) then return 0.2959
+		elseif (speed<2700) then return 0.3027
+		elseif (speed<2800) then return 0.3097
+		elseif (speed<2900) then return 0.3168
+		elseif (speed<3000) then return 0.3243
+		elseif (speed<3100) then return 0.3321
+		elseif (speed<3200) then return 0.3400
+		elseif (speed<3300) then return 0.3482
+		elseif (speed<3400) then return 0.3568
+		elseif (speed<3500) then return 0.3657
+		elseif (speed<3600) then return 0.3748
+		elseif (speed<3700) then return 0.3841
+		elseif (speed<3800) then return 0.3936
+		elseif (speed<3900) then return 0.4030
+		elseif (speed<4000) then return 0.4125
+		elseif (speed<4100) then return 0.4221
+		elseif (speed<4200) then return 0.4320
+		elseif (speed<4300) then return 0.4418
+		else return 0.4516
+		end
+	end
+end
+		
 function SWEP:IsProne()
 	if (not self.Owner:IsPlayer()) then return false end
 	if (not ConVarExists("prone_bindkey_enabled")) then return false end
@@ -2436,10 +2484,21 @@ function SWEP:FlyBulletStart(bullet)
 		miladj=zero/zdata.mils
 		zero=zdata.default
 	end
-	local zerotime=math.floor(((zero*39.3701)/(zerovel*16))/FrameTime()) --amount of frames it will take to fly the distance
-	local drop=0.5*(386*(FrameTime()^2))*(zerotime^2)
+	local drag_speed=zerovel
+	local drag_dist=0
+	local drag_time=0
+	local drag_bc=self.Ammo.coefficient or 0.25
+	local drag_ticks=(GetConVar("kswep_max_flighttime"):GetInt()/engine.TickInterval())
+	while (drag_ticks>0 and drag_dist<zero*39.3701) do
+		drag_ticks=drag_ticks-1
+		drag_time=drag_time+1
+		drag_dist=drag_dist+drag_speed*12*FrameTime()
+		drag_speed=drag_speed+(-1*self:GetDrag("G1",drag_speed)/drag_bc)*drag_speed*FrameTime()
+	end	
+	local zerotime=drag_time --amount of frames it will take to fly the distance
+	local drop=0.5*(386*(FrameTime()^2))*(zerotime^2) --386 inches per second squared gravity
 	local dropadj=math.atan(drop/(zero*39.3701))
-	local scopeang=Vector(0,0,math.sin(dropadj))
+	local scopeang=Vector(0,0,math.sin(dropadj-0.0005))
 	if (zdata.mils) then
 		scopeang=scopeang+Vector(0,0,math.sin(miladj/1000))
 	end
@@ -2447,8 +2506,10 @@ function SWEP:FlyBulletStart(bullet)
 	shot.ticks=(GetConVar("kswep_max_flighttime"):GetInt()/engine.TickInterval())
 	shot.pos=bullet.Src
 	shot.speed=self.Ammo.velocity*self.MuzzleVelMod*supmod
+	print(shot.speed,zerovel)
 	shot.ang=bullet.Dir+scopeang
 	shot.bullet=bullet
+	shot.bc=self.Ammo.coefficient or 0.25
 	shot.dmg=bullet.Damage
 	shot.dist = nil
 	shot.time = CurTime()
@@ -2463,7 +2524,7 @@ function SWEP:FlyBullet(shot)
 	if (shot.dist~=nil) then
 		travel=shot.dist
 	else
-		travel = shot.pos + (shot.ang*shot.speed*16*FrameTime())-Vector(0,0,shot.gravity)
+		travel = shot.pos + (shot.ang*shot.speed*12*FrameTime())-Vector(0,0,shot.gravity)
 	end
 	local tr = util.TraceLine( {
 		filter = self.Owner,
@@ -2483,6 +2544,7 @@ function SWEP:FlyBullet(shot)
 			local armor=0
 			shot.speed, shot.pos, shot.dist=self:CalcPenetration(tr.MatType,shot,tr.HitPos+(tr.Normal*2),travel,tr.HitTexture,tr.Entity)
 		else
+			shot.speed=shot.speed+(-1*self:GetDrag("G1",shot.speed)/shot.bc)*shot.speed*FrameTime()
 			--386 inches per second also thanks justarandomgeek
 			shot.gravity=shot.gravity+(386*(FrameTime()^2))
 			shot.pos=travel
