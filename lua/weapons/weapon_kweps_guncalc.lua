b8a8aec2219c5c044fe11fc5a23d4af7e45e5555
vurtual <VurtualRuler98@gmail.com>
2017-01-24 20:28:52 -0600

    Ballistic calculator now uses kswep BC stuff

--- lua/weapons/weapon_kweps_guncalc.lua
+++ lua/weapons/weapon_kweps_guncalc.lua
@@ -64,13 +64,16 @@ function SWEP:Reload()
 			cbox:SetValue("Select firearm")
 			for k,v in pairs(LocalPlayer():GetWeapons()) do
 				if (string.find(v:GetClass(),"weapon_kswep") and v.Zerodata.mils) then
-					cbox:AddChoice(v.PrintName,Vector(v.Ammo.velocity*v.MuzzleVelMod,v.Zerodata.default,0))
+					local bc=v.Zerodata.bc
+					if (v.Zerodata.bc==-1) then bc=v.Zerodata.Ammo.coefficient or 0.25 end
+					cbox:AddChoice(v.PrintName,Vector(v.Ammo.velocity*v.MuzzleVelMod,v.Zerodata.default,bc))
 				end
 			end
 				
 			function cbox:OnSelect(index,value,data)
 				wep.Gun.Velocity=data.x
 				wep.Gun.Zero=data.y
+				wep.Gun.Coefficient=data.z
 				wep.HasGun=true
 				RangeInputLabel:SetText(wep:CalcDrop())
 			end
@@ -80,7 +83,18 @@ function SWEP:Reload()
 end
 function SWEP:CalcDrop()
 		if (not self.HasGun) then return "no data" end
-		local zerotime=math.floor(((self.Gun.Zero*39.3701)/(self.Gun.Velocity*16))/FrameTime())
+		local drag_speed=self.Gun.Velocity
+		local drag_dist=0
+		local drag_time=0
+		local drag_bc=self.Gun.Coefficient
+		local drag_ticks=(GetConVar("kswep_max_flighttime"):GetInt()/engine.TickInterval())
+		while (drag_ticks>0 and drag_dist<zero*39.3701) do
+			drag_ticks=drag_ticks-1
+			drag_time=drag_time+1
+			drag_dist=drag_dist+drag_speed*12*FrameTime()
+			drag_speed=drag_speed+(-1*self:GetDrag("G1",drag_speed)/drag_bc)*drag_speed*FrameTime()
+		end	
+		local zerotime=drag_time --amount of frames it will take to fly the distance
 		local drop=0.5*(386*(FrameTime()^2))*(zerotime^2)
 		local dropadj=math.atan(drop/(self.Gun.Zero*39.3701))
 		zerotime=math.floor(((self.GunRange*39.3701)/(self.Gun.Velocity*16))/FrameTime())
