e31de7b3f67254667240acc7cb720e4833ca6859
vurtual <VurtualRuler98@gmail.com>
2017-02-07 20:08:49 -0600

    New pouches

--- lua/autorun/kswep_lbeaddon.lua
+++ lua/autorun/kswep_lbeaddon.lua
@@ -12,8 +12,18 @@ tbl.slots = {}
 tbl.addon="none"
 KswepAddLBEAddon(tbl)
 local tbl={}
-tbl.name="ALICE Pistol Addon"
-tbl.slots = {{Pistol1=4,Pistol2=2}}
+tbl.name="ALICE 1911 Mag Pouches 2x"
+tbl.slots = {{Pistol1=4}}
+tbl.addon="ALICE"
+KswepAddLBEAddon(tbl)
+local tbl={}
+tbl.name="ALICE 9mm Mag Pouches 2x"
+tbl.slots = {{PistolCarbine=2,Pistol2=2}}
+tbl.addon="ALICE"
+KswepAddLBEAddon(tbl)
+local tbl={}
+tbl.name="ALICE Shotgun Pouches 2x"
+tbl.slots = {{Shotshell=20}}
 tbl.addon="ALICE"
 KswepAddLBEAddon(tbl)
 local tbl={}
@@ -22,7 +32,7 @@ tbl.slots = {{Pistol1=4,Pistol2=4,PistolCarbine=4}}
 tbl.addon="PALS"
 KswepAddLBEAddon(tbl)
 local tbl={}
-tbl.name="PALS Eagle Shotgun Pouches"
+tbl.name="PALS Eagle Shotgun Pouches 2x"
 tbl.slots = {{Shotshell=48}}
 tbl.addon="PALS"
 KswepAddLBEAddon(tbl)
