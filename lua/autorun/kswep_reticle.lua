6124ba6f2e2350d91c71257685316c2bc4d1080b
vurtual <VurtualRuler98@gmail.com>
2017-06-02 17:32:27 -0500

    Illumination fix for leupold mildot

--- lua/autorun/kswep_reticle.lua
+++ lua/autorun/kswep_reticle.lua
@@ -44,10 +44,10 @@ AddKswepReticle(tbl,"leupold_m151")
 local tbl = {
 {shape="line",start={-200,0},endpos={200,0}},
 {shape="line",start={0,-200},endpos={0,200}},
-{shape="rect",start={-200,-0.25},endpos={-5,0.25}},
-{shape="rect",start={5,-0.25},endpos={200,0.25}},
-{shape="rect",start={-0.25,5},endpos={0.25,200}},
-{shape="rect",start={-0.25,-200},endpos={0.25,-5}}
+{shape="rect",start={-200,-0.25},endpos={-5,0.25},color=color_black},
+{shape="rect",start={5,-0.25},endpos={200,0.25},color=color_black},
+{shape="rect",start={-0.25,5},endpos={0.25,200},color=color_black},
+{shape="rect",start={-0.25,-200},endpos={0.25,-5},color=color_black}
 }
 for i=1,4 do
 	table.insert(tbl,{shape="circle",pos={-i,0},radius=0.1})
