6bc16598f076ad2ae935afe73eea59b3e81263d4
vurtual <VurtualRuler98@gmail.com>
2017-07-21 18:39:06 -0500

    Cheaple hunt!

--- lua/entities/npc_kswep_snipertarget.lua
+++ lua/entities/npc_kswep_snipertarget.lua
@@ -4,6 +4,9 @@ ENT.Spawnable = false
 ENT.Panic=false
 ENT.WanderTime=0
 ENT.StartPos=Vector()
+ENT.RandomPos=false
+ENT.Cheaple=false
+ENT.CheapleMinion=false
 function ENT:Initialize()
 	self:SetModel("models/breen.mdl")
 	self.StartPos=self:GetPos()
@@ -12,6 +15,9 @@ function ENT:Initialize()
 		self:SetHealth(8)
 		self.loco:SetStepHeight(18)
 	end
+	self.RandomPos=GetConVar("kswep_npc_randompos"):GetBool()
+	self.Cheaple=GetConVar("kswep_npc_cheaplehunt"):GetBool()
+	self.CheapleSpawn=self.Cheaple
 end
 function ENT:OnInjured(dmginfo)
 	self.Panic=dmginfo:GetAttacker()
@@ -33,15 +39,70 @@ function ENT:Wander(pos)
 		coroutine.yield()
 	end
 end
+function ENT:OnRemove()
+	if (self.Cheaple and not self.CheapleMinion and self.CheapleTableMinion) then
+		for k,v in pairs(self.CheapleTableMinion) do
+			if (IsValid(v)) then
+				if (self:Health()<1) then
+					v.Panic=true
+					v.Cheaple=false
+				else
+					v:Remove()
+				end
+			end
+		end
+	end
+end
+function ENT:SetCurrentWeaponProficiency()
+end
 function ENT:RunBehaviour()
 	while (true) do
+		local randrange=8192
+		if (self.CheapleSpawn) then
+			if (self.CheapleMinion) then
+				--randrange=1024 causes NPC jams
+				local gender="Male"
+				local maxmodel=9
+				if (math.random()>0.5) then
+					gender="Female"
+					maxmodel=6
+					maxmodel=math.random(1,maxmodel)
+					if (maxmodel==6) then maxmodel=7 end
+				else
+					maxmodel=math.random(1,maxmodel)
+				end
+				self:SetModel("models/Humans/Group01/"..gender.."_0"..maxmodel..".mdl")
+				
+			else
+				self:SetModel("models/Humans/Group01/Male_Cheaple.mdl")
+				self.CheapleTableMinion={}
+				for i=2,5 do
+					local ent=ents.Create("npc_kswep_snipertarget")
+					ent:SetPos(self:GetPos())
+					ent:Spawn()
+					ent.CheapleMinion=true
+					ent.RandomPos=true
+					table.insert(self.CheapleTableMinion,ent)
+				end
+			end
+			self.CheapleSpawn=false
+		end
+	
+		if (self.RandomPos) then
+			local newpos=self:FindSpot("random",{pos=self.StartPos,radius=randrange,stepup=18,stepdown=18})
+			if (newpos) then
+				self:SetPos(newpos)
+				self.StartPos=newpos
+			end
+			self.RandomPos=false
+		end
 		self:StartActivity(ACT_IDLE)
 		local wander=GetConVar("kswep_npc_wander"):GetBool()
 		if (wander and not self.Panic and self.WanderTime<CurTime()) then
 			self:StartActivity(ACT_WALK)
 			self.loco:SetDesiredSpeed(math.random(50,100))
-			local newpos=self:GetPos()+(Vector(math.Rand(-10,10),math.Rand(-10,10),0)*32)
-			if (newpos:Distance(self.StartPos)>256) then
+			local newpos=self:FindSpot("random",{ pos = self.StartPos, radius = 1024, stepup=18, stepdown=18})
+			if (not newpos or newpos:Distance(self.StartPos)>1024) then
 				newpos=self.StartPos
 			end
 			self:Wander(newpos)
@@ -56,12 +117,17 @@ function ENT:RunBehaviour()
 			end
 			self:StartActivity(ACT_RUN)
 			self.loco:SetDesiredSpeed(math.random(150,200))
-			local newpos=self:FindSpot("near",{ type = "hiding", pos = self.StartPos, radius = 8192, stepup=18, stepdown=18})
+			local newpos=self:FindSpot("random",{ type = "hiding", pos = self.StartPos, radius = 8192, stepup=18, stepdown=18})
 			if (newpos) then
 				self:MoveToPos(newpos)
 			end
 			self:PlaySequenceAndWait("fear_reaction")
-			self:Remove()
+			if (self.Cheaple) then
+				self.RandomPos=true
+				self.Panic=false
+			else
+				self:Remove()
+			end
 		end
 		coroutine.yield()
 	end
