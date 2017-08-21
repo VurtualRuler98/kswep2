2b974a7e3cd4c56d969ae3cb94fed17a67762fa5
vurtual <VurtualRuler98@gmail.com>
2017-08-21 11:41:07 -0500

    Upgraded AWM

--- lua/weapons/weapon_kswept_awm.lua
+++ lua/weapons/weapon_kswept_awm.lua
@@ -1,5 +1,5 @@
 --[[
-Copyright 2015 vurtual 
+Copyright 2017 vurtual 
 VurtualRuler98@gmail.com
 vurtual.org
 
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ]]--
 
-
+--Receiver--
 if (SERVER) then
 	AddCSLuaFile()
 end
@@ -27,88 +27,97 @@ if (CLIENT) then
 	SWEP.Slot = 2
 	SWEP.SlotPos = 0
 end
-SWEP.Anims = SWEP.Anims or {}
-SWEP.Category = "KSwep Primary"
-SWEP.IronSightHeight=2 --wild guess again
-SWEP.Base = "weapon_kswep"
-SWEP.Primary.Delay = 1.4
-SWEP.Primary.Spread = 0.01
-SWEP.Spawnable = true
-SWEP.DrawOnce=false
-SWEP.DefaultZerodata= {
-	mils=10,
-	bc=-1,
-	min=0,
-	max=150,
-	step=1,
-	default=100,
-	battlesight=false
-}
 SWEP.AdminSpawnable = true
-SWEP.MagClass="BoltMag"
-SWEP.ViewModel = "models/weapons/cstrike/c_snip_awp.mdl"
---SWEP.WorldModel = "models/weapons/w_mk18.mdl"
-SWEP.WorldModel = "models/weapons/w_snip_awp.mdl"
-SWEP.LoweredOffset = 2
-SWEP.UseHands = true
-SWEP.MuzzleVelMod=1
-SWEP.MagSize = 5
-SWEP.Primary.ClipSize = SWEP.MagSize
-SWEP.Caliber = "vammo_300wm"
-SWEP.Primary.Sound = Sound("weapon_kswept_awm.single")
-SWEP.Primary.SoundSup = Sound("Weapon_kswep.singlesilenced")
-SWEP.ViewModelFlip = false
-SWEP.Secondary.Ammo = ""
-SWEP.CurrentlyReloading=0
-SWEP.ReloadAnimTime=0
-SWEP.ScopeMat="kswep/kswep_scope"
-SWEP.RecoilMassModifier=0.10
-SWEP.HandlingModifier=200
-SWEP.InsAnims=false
-SWEP.Auto=false
-SWEP.Firemode=true
-SWEP.HoldType="ar2"
-SWEP.HoldOpen=false
+SWEP.Base = "weapon_kswep"
+SWEP.Category = "KSwep Primary"
+SWEP.Spawnable = true
+list.Add("NPCUsableWeapons", { class = "weapon_kswept_awm", title = "TEST Arctic Warfare Magnum" } )
+
+--Barrel--
+SWEP.HandlingModifier=1
 SWEP.Length=40
-SWEP.LengthSup=10
-SWEP.Suppressable=false
-SWEP.MuzzleVelModSup= 1.01
-SWEP.RecoilModSup=0.9
-SWEP.SpreadModSup=-0.0005
-SWEP.IdleType="passive"
-SWEP.SelectFire=false
+SWEP.Primary.Spread = 0.01
+SWEP.RecoilMassModifier=0.10
+SWEP.MuzzleVelMod=1
+
+
+--Magazine--
+SWEP.Caliber = "vammo_300wm"
+SWEP.MagClass="BoltMag"
+SWEP.MagSize = 5
 SWEP.MagType="AWM300"
-SWEP.HKBurst=true
-SWEP.IronSightsPos = Vector()
-SWEP.IronSightsAng = Vector()
-SWEP.InsNoIronAnim=false
-SWEP.AimNoModel=true
---SWEP.AimLuaReticle="steiner_scr"
-SWEP.AimLuaReticleMode=true
-SWEP.RTScope=true
-SWEP.ScopeFOV=1.394
-SWEP.ScopeFOVMin=1.394
-SWEP.ScopeFOVMax=6.875
-SWEP.ScopeFOVSteps=12
-SWEP.MinSensitivity=3
-SWEP.MaxSensitivity=15
-SWEP.Scope2DBorderRatio=1.05
-SWEP.Scope2DWheelElevation=true
-SWEP.MergeAttachments = {
-	--foregrip = "models/weapons/upgrades/a_standard_akm.mdl"
- }
-SWEP.InsAttachments=false
-SWEP.Anims.InitialDrawAnim=ACT_VM_READY
-SWEP.CanFlashlight=true
-SWEP.LAMAttachment="1"
-SWEP.NoLowerAnim=true
 function SWEP:ReloadAct(force)
 	self:ReloadMag(force)
 end
-function SWEP:Initialize2()
-	self.ScopeReticleIllumination=Color(255,0,0,255)
+
+--Rail--
+SWEP.BipodHeight=12
+SWEP.CanFlashlight=true
+SWEP.Has2DOptics=true
+SWEP.LAMAttachment="1"
+
+
+--Sight--
+SWEP.IronSightHeight=2 --wild guess again
+SWEP.IronSightsPos = Vector(-7.375,4,0)
+SWEP.IronSightsAng = Vector()
+SWEP.InsAttachments=false
+SWEP.ScopeMat="kswep/kswep_scope"
+function SWEP:InitScopeData(def)
+	def.fovmin=11
+	def.fovmax=11
+	def.sensitivity=1
+	def.name="Default"
+	def.minsensitivity=1
+	def.scopeheight=0
+	def.zero = {
+		mils=false,
+		bc=-1,
+		min=0,
+		max=0,
+		step=0,
+		default=100,
+		battlesight=false
+	}
+	def.windage={mils=false,max=0,step=0}
+	def.windagealt=def.windage
+	def.scope_border=1.1
+	def.scope_ewheel=false
+	def.retcolor=color_black
+	def.luareticle="irons"
+	def.luaretsfp=2.5
+	def.aimmag=6.5
+	def.style="crosshair"
 	if (CLIENT) then self:SetOptic2D("Schmidt & Bender 3-12x50 PM II") end
 end
+
+
+--Stock--
+SWEP.DrawOnce=false
+SWEP.HoldType="ar2"
+SWEP.IdleType="passive"
+SWEP.InsAnims=false
+SWEP.NoLowerAnim=true
+SWEP.LoweredOffset = 2
+SWEP.Primary.Sound = Sound("weapon_kswept_awm.single")
+SWEP.UseHands = true
+SWEP.WorldModel = "models/weapons/w_snip_awp.mdl"
+SWEP.ViewModel = "models/weapons/cstrike/c_snip_awp.mdl"
+SWEP.ViewModelFlip = false
+function SWEP:InitAnims(tbl)
+	tbl.InitialDrawAnim=ACT_VM_READY
+end
+
+
+--Trigger--
+SWEP.Auto=false
+SWEP.Firemode=false
+SWEP.NPCBurstDist=1
+SWEP.NPCBurstMin=1
+SWEP.NPCBurstMax=1
+SWEP.HoldOpen=false
+SWEP.Primary.Delay = 1.4
+SWEP.SelectFire=false
 function SWEP:PrimaryFire()
 	self:NormalFire()
 end
